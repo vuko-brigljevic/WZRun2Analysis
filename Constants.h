4de817ce8dd0260f9026b123bebe1673b3984c19
Sasa Micanovic <smicanov@cern.ch>
2015-08-23 23:35:23 +0200

    added additional constants

---
+++ Constants.h
@@ -0,0 +1,37 @@
+// ### Preselection ###
+
+// electrons
+#define ELELOOSE_BIT 1
+#define ELEMEDIUM_BIT 2
+#define ELE_PTMIN 10.
+#define ELE_ETAMIN 2.5
+
+// muons
+#define MULOOSE_RELISOMIN 0.2
+#define MUTIGHT_RELISOMIN 0.12
+#define MU_PTMIN 10.
+#define MU_ETAMIN 2.4
+
+// general
+#define N_TIGHTLEPTONS 3
+
+
+// ### Z selection ###
+
+#define MZ_MIN 60.
+#define MZ_MAX 120.
+#define PDG_ZMASS 91.1876
+#define ZLEADINGLEPTON_PTMIN 20.
+
+
+// ### W selection ###
+
+#define WLEPTON_PTMIN 20.
+#define WZ_DELTARMIN 0.1
+
+
+// ### MET ###
+
+#define METMIN 30.
+
+
